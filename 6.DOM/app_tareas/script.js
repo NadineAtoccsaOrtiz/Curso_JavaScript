958095a7f0ca442778ce966575d7116115a09f60
joryrod <joryrodriguez25@gmail.com>
2024-12-04 21:21:23 -0500

    crear boton para eliminar un elemento dentro de mi lista

--- 6.DOM/app_tareas/script.js
+++ 6.DOM/app_tareas/script.js
@@ -1,31 +1,17 @@
-let input=document.querySelector("#tarea")
-let button=document.querySelector("#btn-agregar")
-let ul=document.querySelector("#lista-tareas")
+let input = document.querySelector("#tarea");
+let button = document.querySelector("#btn-agregar");
+let ul = document.querySelector("#lista-tareas");
 
-button.addEventListener("click",()=>{
-    // let li=document.createElement("li")
-    // li.textContent=input.value
-
-    //mas facil de crear codigo de html
-    ul.innerHTML = `
-        <li>${input.value}<button class="delete">Eliminar</button></li>
-    `;
-    input.value=""
-
-    let deleteButton = document.querySelector("#delete");
+button.addEventListener("click", () => {
+    let li = document.createElement("li");
+    li.innerHTML = `${input.value} <button class="delete">Eliminar</button>`;
+    
+    input.value = "";
+    
+    let deleteButton = li.querySelector(".delete");
     deleteButton.addEventListener("click", () => {
         ul.removeChild(li);
     });
-    li.appendChild(deleteButton);
-    ul.appendChild(li)
-
-})
-
-
-
-// let eliminarBtn = document.createElement("button");
-    // eliminarBtn.textContent = "Eliminar";
-    // eliminarBtn.addEventListener("click", () => {
-    //     ul.removeChild(li);
-    // });
-    //li.appendChild(eliminarBtn);
+    
+    ul.appendChild(li); 
+});
