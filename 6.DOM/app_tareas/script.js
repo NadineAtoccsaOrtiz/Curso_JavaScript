2d7b01e1bce4c15a7f9dd808d28ab08cc6c04c7c
Nadine <nadineatoccsaortiz@gmail.com>
2024-12-11 09:17:32 -0500

    crear boton editar en js

--- 6.DOM/app_tareas/script.js
+++ 6.DOM/app_tareas/script.js
@@ -4,14 +4,21 @@ let ul = document.querySelector("#lista-tareas");
 
 button.addEventListener("click", () => {
     let li = document.createElement("li");
-    li.innerHTML = `${input.value} <button class="delete">Eliminar</button>`;
-    
+    li.innerHTML = `${input.value} <button class="delete">Eliminar</button> <button class="edit">Editar</button>`;
     input.value = "";
-    
     let deleteButton = li.querySelector(".delete");
+    let editButton = li.querySelector(".edit");
+    
     deleteButton.addEventListener("click", () => {
-        ul.removeChild(li);
+      ul.removeChild(li);
     });
     
-    ul.appendChild(li); 
-});
+    editButton.addEventListener("click", () => {
+      let newText = prompt("Ingrese el nuevo texto:");
+      li.innerHTML = `${newText} <button class="delete">Eliminar</button> <button class="edit">Editar</button>`;
+    });
+    
+    ul.appendChild(li);
+  });
+
+
